6b657c94df719862e06af1e8ad78d7f8c36cb8e2
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2020-11-16 20:17:38 -0700

    add poll types

--- src/interfaces/polls.ts
+++ src/interfaces/polls.ts
@@ -1,1 +1,15 @@
-export interface Polls {}
+enum VotingSystems {
+	FirstPastThePost = 1,
+	RankedChoice = 2,
+	SingleTransferable = 3,
+}
+
+export interface Poll {
+	id: string;
+	title: string;
+	description?: string;
+	choices: string[];
+	system: VotingSystems;
+}
+
+export type Polls = Poll[];
