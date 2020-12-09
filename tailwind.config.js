23edf037971e5d68e5c36acff3c36eee3a5f5a85
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2020-12-09 06:39:55 -0700

    replace manual container plugin w/ my npm package!

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,34 +31,5 @@ module.exports = {
 		},
 	},
 	variants: {},
-	plugins: [
-		require(`@tailwindcss/forms`),
-		function ({ addComponents }) {
-			addComponents({
-				'.container': {
-					width: `100%`,
-					marginLeft: `auto`,
-					marginRight: `auto`,
-					paddingLeft: `1rem`,
-					paddingRight: `1rem`,
-					'@screen sm': {
-						paddingLeft: `1.5rem`,
-						paddingRight: `1.5rem`,
-					},
-					'@screen md': {
-						paddingLeft: `3rem`,
-						paddingRight: `3rem`,
-					},
-					'@screen lg': {
-						paddingLeft: `6rem`,
-						paddingRight: `6rem`,
-					},
-					'@screen xl': {
-						paddingLeft: `9rem`,
-						paddingRight: `9rem`,
-					},
-				},
-			});
-		},
-	],
+	plugins: [require(`@tailwindcss/forms`), require(`caleb-tw-plugins`)],
 };
