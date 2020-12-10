e8352171cce363c927a1f5e92fa8a2cc573cfe70
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2020-12-09 23:31:48 -0700

    update 404 to ts

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
-export default function Custom404() {
+const Custom404 = (): JSX.Element => {
 	return (
 		<main className='container flex items-center justify-center w-full bg-base-primary min-h-content text-base-secondary'>
-			<div className='flex items-center justify-center min-h-content-area text-text-base'>
+			<div className='flex items-center justify-center mb-10 min-h-content-area text-text-base'>
 				<div className='flex flex-col items-center justify-center sm:flex-row'>
 					<h1 className='px-2 pb-4 mb-4 text-4xl border-b-2 sm:border-b-0 sm:border-r-2 border-text-base sm:px-0 sm:mb-0 sm:pb-2 sm:pt-2 sm:mr-4 sm:pr-4'>
 						404
@@ -11,4 +11,6 @@ export default function Custom404() {
 			</div>
 		</main>
 	);
-}
+};
+
+export default Custom404;
