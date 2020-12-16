645995142d0659ebb9d8a6cece4649fcbec63eb5
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2020-12-16 03:55:47 -0700

    return new poll data on POST success

--- src/pages/api/polls.ts
+++ src/pages/api/polls.ts
@@ -3,27 +3,33 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { db } from '@utils/firebase-admin';
 
 export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
-	if (req.method === `POST`) {
-		try {
-			const formData = req.body;
-			const pollsRef = db.collection(`polls`);
-			const newPollSnapshot = await pollsRef.add(formData);
-			const newPoll = await newPollSnapshot.get();
-			res.status(200).json({ data: newPoll.data() });
-		} catch (error) {
-			res.status(500).json({ error });
-		}
-	} else if (req.method === `GET`) {
-		try {
-			const pollsRef = db.collection(`polls`);
-			const snapshot = await pollsRef.get();
-			const polls = [];
-			snapshot.forEach(doc => {
-				polls.push({ id: doc.id, ...doc.data() });
-			});
-			res.status(200).json(polls);
-		} catch (error) {
-			res.status(500).json({ error });
-		}
+	const { method, body } = req;
+	switch (method) {
+		case `GET`:
+			try {
+				const pollsRef = db.collection(`polls`);
+				const snapshot = await pollsRef.get();
+				const polls: any[] = [];
+				snapshot.forEach(doc => {
+					polls.push({ id: doc.id, ...doc.data() });
+				});
+				res.status(200).json(polls);
+			} catch (error) {
+				res.status(500).json({ error });
+			}
+			break;
+		case `POST`:
+			try {
+				const pollsRef = db.collection(`polls`);
+				const newPoll = await pollsRef.add({ ...body, createdAt: new Date().toISOString() });
+				const snapshot = await newPoll.get();
+				res.status(200).json({ id: snapshot.id, ...snapshot.data() });
+			} catch (error) {
+				res.status(500).json({ error });
+			}
+			break;
+		default:
+			res.setHeader(`Allow`, [`GET`, `POST`]);
+			res.status(405).end(`Method ${method} Not Allowed`);
 	}
 };
