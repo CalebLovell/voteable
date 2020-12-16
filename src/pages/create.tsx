7de821c9ee8cc49f44c30905f384975073ee7c09
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2020-12-16 03:57:09 -0700

    add loading spinner, parse data & push to route

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -5,28 +5,32 @@ import { FieldArray } from 'react-final-form-arrays';
 import { PollSchema } from '@utils/dataSchemas';
 import arrayMutators from 'final-form-arrays';
 import { logError } from '@utils/logError';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 import { useToasts } from 'react-toast-notifications';
 
 export default function CreatePage(): JSX.Element {
 	const { addToast } = useToasts();
+	const [loading, setLoading] = useState(false);
 	const user_id = `0ngT8eAMT9mNH5qyqSem`;
+	const router = useRouter();
 
-	const onSubmit = async (data: unknown) => {
+	const onSubmit = async (formData: unknown) => {
 		try {
-			assert(data, PollSchema);
-			const postData = async () => {
-				const res = await fetch(`http://localhost:3000/api/polls`, {
-					method: `POST`,
-					headers: { 'Content-Type': `application/json` },
-					body: JSON.stringify(data),
-					mode: `cors`,
-					referrerPolicy: `no-referrer`,
-				});
-				return res;
-			};
-			const res = await postData();
+			assert(formData, PollSchema);
+			setLoading(true);
+			const res = await fetch(`http://localhost:3000/api/polls`, {
+				method: `POST`,
+				headers: { 'Content-Type': `application/json` },
+				body: JSON.stringify(formData),
+				mode: `cors`,
+				referrerPolicy: `no-referrer`,
+			});
+			const data = await res.json();
+			setLoading(false);
 			if (res.status === 200) {
 				addToast(`Saved Successfully`, { appearance: `success` });
+				router.push(`/polls/${data.id}`);
 			} else {
 				addToast(res.statusText, { appearance: `error` });
 			}
@@ -252,6 +256,16 @@ export default function CreatePage(): JSX.Element {
 							className='inline-flex items-center px-3 py-2 mt-4 text-sm font-medium leading-4 text-black bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
 						>
 							Submit
+							{loading ? (
+								<svg className='w-4 h-4 ml-2 text-accent-primary animate-spin' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24'>
+									<circle className='opacity-25' cx='12' cy='12' r='10' stroke='currentColor' strokeWidth='4'></circle>
+									<path
+										className='opacity-75'
+										fill='currentColor'
+										d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+									></path>
+								</svg>
+							) : null}
 						</button>
 					</form>
 				)}
