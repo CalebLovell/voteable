6f80f7618a83ea0d5e269b73966c976d7709e7c7
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2020-12-19 20:54:58 -0700

    update schema to with my issue fixed

--- src/utils/dataSchemas.ts
+++ src/utils/dataSchemas.ts
@@ -2,15 +2,7 @@ import { Describe, array, enums, object, optional, size, string } from 'superstr
 
 import { NewPollData } from './dataTypes';
 
-export const BasePollSchema: Describe<NewPollData> = object({
-	title: string(),
-	description: optional(string()),
-	choices: array(string()),
-	types: array(enums([`First Past The Post`, `Ranked Choice`, `Single Transferable`])),
-	user_id: string(),
-});
-
-export const PollSchema = object({
+export const PollSchema: Describe<NewPollData> = object({
 	title: size(string(), 1, 100),
 	description: optional(size(string(), 1, 2000)),
 	choices: size(array(string()), 1, 10),
